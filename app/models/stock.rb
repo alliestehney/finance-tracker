d23196e6e928418a079fa765dc7e0a0c2f7480ce
alliestehney <allie@quikly.com>
2019-06-26 20:20:37 -0400

    Add stock restrictions

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,7 +5,7 @@ class Stock < ApplicationRecord
   def self.new_from_lookup(ticker_symbol)
     # construct a new stock object and send back to who is calling it
     begin
-      StockQuote::Stock.new(api_key: 'ADD API KEY (saved locally)')
+      StockQuote::Stock.new(api_key: 'API KEY')
       looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
       price = looked_up_stock.latest_price
       new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: price)
@@ -13,4 +13,9 @@ class Stock < ApplicationRecord
       return nil
     end
   end
+
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
+  end
+
 end
