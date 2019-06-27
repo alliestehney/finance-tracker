4e6ae1da8b2974477d7e431382dff3168da9f9b6
alliestehney <allie@quikly.com>
2019-06-26 20:15:35 -0400

    Get StockQuote working again with API key

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,6 +5,7 @@ class Stock < ApplicationRecord
   def self.new_from_lookup(ticker_symbol)
     # construct a new stock object and send back to who is calling it
     begin
+      StockQuote::Stock.new(api_key: 'ADD API KEY (saved locally)')
       looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
       price = looked_up_stock.latest_price
       new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol, last_price: price)
@@ -12,8 +13,4 @@ class Stock < ApplicationRecord
       return nil
     end
   end
-
-  # def self.strip_commas(number)
-  #   number.gsub(",", "")
-  # end
 end
